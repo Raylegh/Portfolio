025d8f36aa0ff3de1ec7d7268340ffbb06b9fab7
Santi Sellares <santisellares10@gmail.com>
2021-02-16 22:30:42 +0100

    Skills 1.0

--- src/components/About/SkillsSection.js
+++ src/components/About/SkillsSection.js
@@ -8,8 +8,51 @@ const SkillsSection = () => {
       </div>
 
       <div className="skills">
-        <h4>phoshotp</h4>
-        <h4>illustrator</h4>
+        <div className="skill">
+          <div className="skill-info">
+            <h4>Photoshop</h4>
+            <div className="skill-val">
+              <span>95%</span>
+            </div>
+          </div>
+          <div className="skill-but">
+            <div className="skill-hold">
+              <div className="skill-bar">{/* WIDTH 90% */}</div>
+            </div>
+          </div>
+        </div>
+
+        {/* just for test */}
+
+        <div className="skill">
+          <div className="skill-info">
+            <h4>Ilustrator</h4>
+            <div className="skill-val">
+              <span>95%</span>
+            </div>
+          </div>
+          <div className="skill-but">
+            <div className="skill-hold">
+              <div className="skill-bar">{/* WIDTH 90% */}</div>
+            </div>
+          </div>
+        </div>
+
+        {/* just for test */}
+
+        <div className="skill">
+          <div className="skill-info">
+            <h4>ilustrator</h4>
+            <div className="skill-val">
+              <span>95%</span>
+            </div>
+          </div>
+          <div className="skill-but">
+            <div className="skill-hold">
+              <div className="skill-bar">{/* WIDTH 90% */}</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
