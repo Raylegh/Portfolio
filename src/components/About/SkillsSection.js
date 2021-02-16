f3bbd457a937b81c1dd56aa02751958c410b7f41
Santi Sellares <santisellares10@gmail.com>
2021-02-16 23:56:49 +0100

    Skills 1.1

--- src/components/About/SkillsSection.js
+++ src/components/About/SkillsSection.js
@@ -1,3 +1,6 @@
+//Styles for keyframes
+import styled, { keyframes } from "styled-components";
+
 const SkillsSection = () => {
   return (
     <div className="skill-section">
@@ -6,52 +9,46 @@ const SkillsSection = () => {
           Main <span>Skills</span>
         </h2>
       </div>
-
       <div className="skills">
-        <div className="skill">
-          <div className="skill-info">
-            <h4>Photoshop</h4>
-            <div className="skill-val">
-              <span>95%</span>
-            </div>
-          </div>
-          <div className="skill-but">
-            <div className="skill-hold">
-              <div className="skill-bar">{/* WIDTH 90% */}</div>
-            </div>
-          </div>
+        <Skills skill="Photoshop" note="90%" />
+        <Skills skill="Illustrator" note="95%" />
+        <Skills skill="InDesign" note="80%" />
+        <Skills skill="Clo 3D" note="75%" />
+        <Skills skill="WGSN" note="35%" />
+        <Skills skill="Google ADS" note="55%" />
+        <Skills skill="Facebook ADS" note="65%" />
+        <Skills skill="Google Analytics" note="40%" />
+      </div>
+    </div>
+  );
+};
+
+const Skills = ({ skill, note }) => {
+  //Skill Animation
+  const SkillKey = keyframes`
+    from {
+      width: 0%;
+    }
+    to {
+      width: ${note};
+    }
+  `;
+  const SkillAnimation = styled.div`
+    animation: ${SkillKey} 2s; /* works */
+    width: ${note};
+  `;
+
+  return (
+    <div className="skill">
+      <div className="skill-info">
+        <h4>{skill}</h4>
+        <div className="skill-val">
+          <span>{note}</span>
         </div>
-
-        {/* just for test */}
-
-        <div className="skill">
-          <div className="skill-info">
-            <h4>Ilustrator</h4>
-            <div className="skill-val">
-              <span>95%</span>
-            </div>
-          </div>
-          <div className="skill-but">
-            <div className="skill-hold">
-              <div className="skill-bar">{/* WIDTH 90% */}</div>
-            </div>
-          </div>
-        </div>
-
-        {/* just for test */}
-
-        <div className="skill">
-          <div className="skill-info">
-            <h4>ilustrator</h4>
-            <div className="skill-val">
-              <span>95%</span>
-            </div>
-          </div>
-          <div className="skill-but">
-            <div className="skill-hold">
-              <div className="skill-bar">{/* WIDTH 90% */}</div>
-            </div>
-          </div>
+      </div>
+      <div className="skill-but">
+        <div className="skill-hold">
+          <SkillAnimation className="skill-bar"></SkillAnimation>
         </div>
       </div>
     </div>
